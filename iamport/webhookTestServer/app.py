e934ed3fbed06ab73d1cf5278779aa528d997c33
Sonny <sonny@chai.finance>
2022-04-25 21:16:50 +0900

    First commit

---
+++ iamport/webhookTestServer/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, Response
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/webhook', methods=['GET'])
+def webhook():
+    args = request.args
+    code = args.get('code', default='200', type=str)
+    delay = args.get('delay', default=0, type=int)
+
+    time.sleep(delay)
+
+    return Response("Good!", status=code, mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run()
