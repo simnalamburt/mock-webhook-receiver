f5d68696864ccc3f86f89fa14e15c882caf568a3
SonnySon17 <sonny@chai.finance>
2022-04-26 22:05:46 +0900

    divide request methods

--- iamport/webhookTestServer/app.py
+++ iamport/webhookTestServer/app.py
@@ -9,8 +9,15 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/webhook', methods=['GET'])
-def webhook():
+@app.route('/webhook/post/<code>/<delay>', methods=['POST'])
+def webhook_post(code, delay):
+    time.sleep(int(delay))
+
+    return Response("Good!", status=code, mimetype='application/json')
+
+
+@app.route('/webhook/get', methods=['GET'])
+def webhook_get():
     args = request.args
     code = args.get('code', default='200', type=str)
     delay = args.get('delay', default=0, type=int)
@@ -18,6 +25,7 @@ def webhook():
     time.sleep(delay)
 
     return Response("Good!", status=code, mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
